3d75c597080013ca01e095be198ce854973b207f
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-08-29 17:12:32 -0700

    Add express server and GET routes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,2 +1,34 @@
-/* Dependencies */
+/* DEPENDENCIES */
 const express = require('express');
+var path = require('path');
+
+
+
+var notesArray = require("./db/db.json");
+
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+
+app.get("/api/notes", function (req, res) {
+    return res.json(notesArray);
+});
