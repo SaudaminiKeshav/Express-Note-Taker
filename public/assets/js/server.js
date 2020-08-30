e9a6d14edd8f06d5267b63ae69e4a97fc83af0cf
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-08-29 17:15:31 -0700

    Add POST route to save note

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,22 +2,21 @@
 const express = require('express');
 var path = require('path');
 
-
-
 var notesArray = require("./db/db.json");
 
-
+/* SETS UP THE EXPRESS APP */
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-
+/* STARTS THE SERVER TO BEGIN LISTENING */
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
 
 
 app.get("/", function (req, res) {
@@ -31,4 +30,16 @@ app.get("/notes", function (req, res) {
 
 app.get("/api/notes", function (req, res) {
     return res.json(notesArray);
+});
+
+
+
+// Route for saving a note to db.json
+app.post("/api/notes", function (req, res) {
+
+    let newNoteRequest = req.body;
+    console.log(newNoteRequest);
+
+    notesArray.push(newNoteRequest);
+    res.sendStatus(200);
 });
